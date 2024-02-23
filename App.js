cded4b6b0ec3d06f575b12f341cb2bb0c4742f5a
muranakar <P1211065@gmail.com>
2024-02-24 07:00:46 +0900

    first commit1

--- App.js
+++ App.js
@@ -1,20 +1,51 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from "react";
+import { View, Text, StyleSheet } from "react-native";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+class LifecycleExample extends Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
+    this.state = {
+      counter: 0,
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("componentDidUpdate");
+  }
+
+  componentWillUnmount() {
+    console.log("componentWillUnmount");
+  }
+
+  increaseCounter = () => {
+    this.setState((prevState) => ({ counter: prevState.counter + 1 }));
+  };
+
+  render() {
+    console.log("render");
+    return (
+      <View style={styles.container}>
+        <Text onPress={this.increaseCounter}>Click to Increase Counter</Text>
+        <Text style={styles.counterText}>Counter: {this.state.counter}</Text>
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  counterText: {
+    marginTop: 20, // テキストの上に余白を設定する場合
   },
 });
+
+export default LifecycleExample;
